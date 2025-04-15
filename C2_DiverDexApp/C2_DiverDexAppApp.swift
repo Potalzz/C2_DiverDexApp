d2f61adfbc7c075b698b17b89e5b0fff9d3dc904
Potalzz <tprhkd1607@naver.com>
2025-04-15 23:22:01 +0900

    Initial Commit

---
+++ C2_DiverDexApp/C2_DiverDexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2_DiverDexAppApp.swift
+//  C2_DiverDexApp
+//
+//  Created by PenguinLand on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct C2_DiverDexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
