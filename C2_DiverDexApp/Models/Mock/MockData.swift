edda1d5731511abd3c82ccb005d497731ba1be1e
Potalzz <tprhkd1607@naver.com>
2025-04-16 21:38:36 +0900

    [#3] feat: 프로필 디테일 뷰 UI 구현

--- C2_DiverDexApp/Models/Mock/MockData.swift
+++ C2_DiverDexApp/Models/Mock/MockData.swift
@@ -6,16 +6,40 @@
 //
 struct DummyData {
     static let sampleProfileDatas = [
-        ProfileCardModel(name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
-        
-        ProfileCardModel(name: "Green", category: .design, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "Green", category: .design, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "Green", category: .design, ProfileImageName: "profile-1"),
-        
-        ProfileCardModel(name: "Zigu", category: .business, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "iL", category: .business, ProfileImageName: "profile-1"),
-        ProfileCardModel(name: "Presence", category: .business, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Rumy", category: .tech, ProfileImageName: "profile-1"),
+
+        ProfileCardModel(
+            name: "Green", category: .design, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Green", category: .design, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Green", category: .design, ProfileImageName: "profile-1"),
+
+        ProfileCardModel(
+            name: "Zigu", category: .business, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "iL", category: .business, ProfileImageName: "profile-1"),
+        ProfileCardModel(
+            name: "Presence", category: .business, ProfileImageName: "profile-1"
+        ),
+    ]
+
+    static let sampleProfileDetailDatas = [
+        ProfileDetailInfo(
+            name: "김진혁",
+            nickname: "Velko",
+            email: "velkozKing@gmail.com",
+            blog: "velkoz'slog.velog.com",
+            wiki:
+                "https://www.notion.so/posacademy/Velko-1b42b843d5af8137b638ec99dd8e52a2?pvs=4",
+            instagram: "@ilovevelkoz",
+            interests: "벨코즈, 지렁이, 광선, 우주괴물",
+            mbti: "INTP"
+        )
     ]
 }
---
+++ C2_DiverDexApp/Models/ProfileDetailInfo.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileDetailInfo.swift
+//  C2_DiverDexApp
+//
+//  Created by PenguinLand on 4/16/25.
+//
+
+import SwiftUI
+
+struct ProfileDetailInfo: Identifiable {
+    let id = UUID()
+    let name: String
+    let nickname: String
+    let email: String
+    let blog: String
+    let wiki: String
+    let instagram: String
+    let interests: String
+    let mbti: String
+}
